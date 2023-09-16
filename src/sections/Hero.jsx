86a30b2947ef1a3a0c816a58373d0391ba598cf0
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-09-15 22:08:50 -0300

    💄 changes on Hero.jsx component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import { arrowRight } from "../assets/icons"
 import Button from "../components/Button"
+import { statistics } from "../constants"
 
 const Hero = () => {
   return (
@@ -11,18 +12,39 @@ const Hero = () => {
         className="relative xl:w-2/5 flex flex-col justify-center items-start
         w-full max-xl:padding-x pt-28"
       >
-        <p>Our Summer Collection</p>
-        <h1>
-          <span>The New Arrival</span>
+        <p 
+          className="text-xl font-montserrat text-coral-red"
+        >
+          Our Summer Collection
+        </p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
           <br />
-          <span>Nike</span>
-          Shoes
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
-        <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+        <p
+          className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm"
+        >
+          Discover stylish Nike arrivals, quality comfort, and innovation for your active life.
+        </p>
+
         <Button 
           label="Shop now" 
           iconURL={arrowRight}
         />
+
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stac) => (
+            <div key={stac.label}>
+              <p className="text-4xl font-palanquin font-bold">
+                {stac.value}
+              </p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stac.label}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   )
