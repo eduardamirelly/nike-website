c28d8401000def86a932cfca2b28540c524a314b
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-09-15 22:41:06 -0300

    💄 create ShoeCard.jsx component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,8 @@
 import { arrowRight } from "../assets/icons"
+import { bigShoe1 } from "../assets/images"
 import Button from "../components/Button"
-import { statistics } from "../constants"
+import ShoeCard from "../components/ShoeCard"
+import { shoes, statistics } from "../constants"
 
 const Hero = () => {
   return (
@@ -9,8 +11,7 @@ const Hero = () => {
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
       <div
-        className="relative xl:w-2/5 flex flex-col justify-center items-start
-        w-full max-xl:padding-x pt-28"
+        className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28"
       >
         <p 
           className="text-xl font-montserrat text-coral-red"
@@ -42,6 +43,28 @@ const Hero = () => {
               <p className="leading-7 font-montserrat text-slate-gray">
                 {stac.label}
               </p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img 
+          src={bigShoe1} 
+          alt="shoe collection"
+          width={610}
+          height={500}
+          className="object-contain relative z-10"
+        />
+
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%]  sm:left-[10%] max-sm:px-6">
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard
+                imageURL={shoe}
+                changeBigShoeImage={() => {}}
+                bigShoeImg=""
+              />
             </div>
           ))}
         </div>
