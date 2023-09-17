0ab254fee2c025bc1f43470c72c74d6f592d823a
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-09-16 23:12:29 -0300

    💄 changes on SpecialOffer.jsx section

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,6 +1,41 @@
+import { arrowRight } from "../assets/icons"
+import { offer } from "../assets/images"
+import Button from "../components/Button"
+
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className="max-container flex max-xl:flex-col-reverse gap-10 items-center justify-between w-full">
+      <div className="flex-1">
+        <img 
+          src={offer} 
+          alt="special offer"
+          width={773}
+          height={687}
+          className="object-contain w-full"
+        />
+      </div>
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">
+          <span className="text-coral-red"> Special </span> Offer 
+        </h2>
+        <p className="lg:max-w-lg mt-4 info-text">
+          Embark on a shopping journey that redefines your experience with unbeatable deals. From premier selections to incredible saving, we offer unparalleled value that sets us apart.
+        </p>
+        <p className="lg:max-w-lg mt-6 info-text">
+          Navigate a realm of possibilities designed to fulfill your unique desires, surpassing the loftiest expectations. Your journey with us is nothing short of exceptional.
+        </p>
+
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label="Shop now" iconURL={arrowRight} />
+          <Button 
+            label="Learn more"
+            backgroundColor="bg-white"
+            borderColor="border-slate-gray"
+            textColor="text-slate-gray"
+          />
+        </div>
+      </div>
+    </section>
   )
 }
 
