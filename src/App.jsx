6320b3efa4d9351758718bd2ab9f1af2e77bcc0a
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-10-14 21:06:09 -0300

    :dizzy: added animations to Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,7 @@ const App = () => {
   return (
     <main className="relative">
       <Nav />
-      <section className="xl:padding-l wide:padding-r padding-b
-      ">
+      <section className="xl:padding-l wide:padding-r padding-b">
         <Hero />
       </section>
       <section className="padding">
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,25 +2,56 @@ import { headerLogo } from '../assets/images';
 import { hamburger } from '../assets/icons';
 import { navLinks } from '../constants';
 
+import { motion } from "framer-motion"
+
 const Nav = () => {
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
       <nav className="flex justify-between items-center max-container">
         <a href="/">
-          <img
-            src={headerLogo}
-            alt="Logo"
-            width={130}
-            height="29"
-          />
+          <motion.div
+            initial={{
+              opacity: 0,
+              scale: 0.5,
+            }}
+            animate={{
+              opacity: 1,
+              scale: 1,
+            }}
+            transition={{
+              duration: 1,
+            }}
+          >
+            <img
+              src={headerLogo}
+              alt="Logo"
+              width={130}
+              height="29"
+            />
+          </motion.div>
         </a>
         <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
           {navLinks.map((item) => (
-            <li key={item.lable}>
-              <a href={item.href} className="font-montserrat leading-normal text-lg text-slate-gray">
-                {item.label}
-              </a>
-            </li>
+            <motion.div 
+              initial={{
+                opacity: 0,
+                scale: 0.5,
+              }}
+              animate={{
+                opacity: 1,
+                scale: 1,
+              }}
+              transition={{
+                duration: 1,
+              }}
+              key={item.label}
+            >
+              <li>
+                <a href={item.href} className="font-montserrat leading-normal text-lg text-slate-gray">
+                  {item.label}
+                </a>
+              </li>
+            </motion.div>
           ))}
         </ul>
         <div className="hidden max-lg:block">
